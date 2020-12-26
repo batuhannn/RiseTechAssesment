5b373dc7bd62babf60f595a72a6b8f7406215048
batuhannn <batuhanndur@gmail.com>
2020-12-26 14:50:28 +0300

    CommunicationInfoApi
    CommunicationInfoApi get list done.

--- RiseTech/CommunicationInfoApi/Controllers/CommunicationInfoApiController.cs
+++ RiseTech/CommunicationInfoApi/Controllers/CommunicationInfoApiController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Common.Models.DTO;
+using Common.Services.Contracts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -11,6 +13,15 @@ namespace CommunicationInfoApi.Controllers
     [Route("api/CommunicationInfo")]
     public class CommunicationInfoApiController : ControllerBase
     {
-
+        private readonly ICommunicationInfoService _communicationInfoService;
+        public CommunicationInfoApiController(ICommunicationInfoService communicationInfoService)
+        {
+            _communicationInfoService = communicationInfoService;
+        }
+        [HttpGet]
+        public async Task<List<CommunicationInfoDTO>> GetCommunicationInfos() 
+        {
+            return await _communicationInfoService.GetCommunicationInfosList();
+        } 
     }
 }
